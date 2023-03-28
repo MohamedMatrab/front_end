88239fb587015ab82aac423eabdde1a68ed61145
LachhabMeriem <121290183+LachhabMeriem@users.noreply.github.com>
2023-03-28 04:19:51 +0000

    'main_meriem'

---
+++ Controllers/controllers_page.php
@@ -0,0 +1,15 @@
+<?php
+
+    function indexAction() {
+        require_once 'views/home.php' ;
+    }
+
+    function aboutCentreAction() {
+        require_once 'views/aboutCentre.php' ;
+    }
+    function aboutDoctorAction() {
+        require_once 'views/aboutDoctor.php' ;
+    }
+    function appointAction() {
+        require_once 'views/Appointment.php' ;
+    }
