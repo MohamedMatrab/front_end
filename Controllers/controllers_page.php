74f0823af3135366f5b616711c429323cc6e6201
LachhabMeriem <121290183+LachhabMeriem@users.noreply.github.com>
2023-04-02 13:16:07 +0000

    Committing changes to controllers_page.php

---
+++ Controllers/controllers_page.php
@@ -0,0 +1,24 @@
+<?php
+
+    function indexAction() {
+        require_once 'views/home.php' ;
+    }
+
+    function aboutCentreAction() {
+        require_once 'views/aboutCentre.php' ;
+    }
+    function aboutDoctorAction() {
+        require_once 'views/aboutDoctor.php' ;
+    }
+    function appointAction() {
+        require_once 'views/Appointment.php' ;
+    }
+    function loginAction() {
+        require_once 'views/login.php' ;
+    }
+    function portfolioAction() {
+        require_once 'views/portfolio.php' ;
+    }
+    function singupAction() {
+        require_once 'views/singup.php' ;
+    }
