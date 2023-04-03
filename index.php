6df756af05f2671fd414403e95c69059ae9ac513
saidawh <saidawahmane17@gmail.com>
2023-04-03 11:59:10 +0200

    commit done

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dentcare</title>
+    <link rel="stylesheet" href="style.css">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+<body>
+    <!--service secttion-->
+
+
+    <section id="service" class="services pt-0">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-title">
+            <h2>Check our services</h2>
+
+          </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="100">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo5.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Esthétique dentaire</a></h3>
+              <p>Bien entretenir ses dents, c’est en premier lieu le respect des règles d’hygiène...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="200">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo6.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Facette dentaire</a></h3>
+              <p>C’est une fine pellicule en céramique que le praticien colle sur une dent...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="300">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo7.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Implant dentaire</a></h3>
+              <p>L’implant dentaire est la meilleure solution pour imiter la nature...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="400">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo8.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Prothèse dentaire</a></h3>
+              <p>C’est un dispositif qui vise à reconstituer la dent abîmée ou à remplacer...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="500">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo9.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Blanchiment dentaire</a></h3>
+              <p>Vous rêviez d’un sourire lumineux ? Les produits actuels ne cessent de...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="600">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo10.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">parodontologie</a></h3>
+              <p>Les maladies des gencives, aussi connues sous le nom de maladies parodontales...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="700">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo11.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Orthodontie invisible</a></h3>
+              <p>Avoir une dentition parfaite est une idée qui peut être très alléchante pour touts...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="800">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo12.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Pédodontie</a></h3>
+              <p>Les soins dentaires destinés aux enfants constituent une discipline importante...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="900">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo13.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">médecine esthétique</a></h3>
+              <p>Selon l'organisation mondiale de la santé, une bonne santé de définie par...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="1000">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo14.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Soins dentaires</a></h3>
+              <p>Bien entretenir ses dents, c’est en premier lieu le respect des règles d’hygiène...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="1100">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo15.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">Radiologie dentaire</a></h3>
+              <p>La radiographie dentaire est un élément important dans le suivi de votre santé...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+          <div class="col-lg-3 col-md-3" data-aos="fade-up" data-aos-delay="1200">
+            <div class="card">
+              <div class="card-img">
+                <img src="ASSET/spécialité/photo16.jpg" alt="" class="img-fluid">
+              </div>
+              <h3><a href="service_details.php" class="stretched-link">tourisme dentaire</a></h3>
+              <p>Le Maroc est connu depuis longtemps par la qualité des soins que cela soit...</p>
+            </div>
+          </div><!-- End Card Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p> contactez nous pour plus d'informations</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Eddahbi,Gueliz marrakech</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>esthétique@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+212 693452015</p>
+              </div>
+
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3396.979999113054!2d-8.016004!3d31.634398!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdafee8c16aaf64b%3A0x908a00581b0d2694!2sCentre%20dentaire%20Moqadem%20Haf%C3%A7a%20(Hollywood%20smile%2C%20Implant%2C%20proth%C3%A8se%2C%20Facettes%2C%20P%C3%A9dodontie%2C%20Blanchiment)%20Maroc%2C%20Marrakech!5e0!3m2!1sfr!2sma!4v1677161821153!5m2!1sfr!2sma" width="330" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+          </div>
+        </div>
+
+        <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+          <form action="contact.php" method="POST" role="form" class="php-email-form">
+            <div class="row">
+              <div class="form-group col-md-6">
+                <label for="name">Your Name</label>
+                <input type="text" name="name" class="form-control" id="name" required>
+              </div>
+              <div class="form-group col-md-6 mt-3 mt-md-0">
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" name="email" id="email" required >
+              </div>
+            </div>
+            
+            <div class="form-group mt-3">
+              <label for="message">Message</label>
+              <textarea class="form-control" name="message" rows="10" id="message" required ></textarea>
+            </div>
+              
+            
+            <div class="text-center"><button type="submit">Send Message</button></div>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+
+
+
+
+
+
+    
+</body>
+</html>
