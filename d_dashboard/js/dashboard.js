c81ca9dc09fef067b591308f29df0771e2859821
mohamedMatrab <mb200ts@gmail.com>
2023-04-15 15:50:38 +0000

    dashboard version 1

--- d_dashboard/js/dashboard.js
+++ d_dashboard/js/dashboard.js
@@ -1,98 +1,92 @@
-// let Sections = document.querySelectorAll("section div");
-// let sidebarSections = document.querySelectorAll(".disabled");
-// let Dropdown = [["toutes les résérvations","historique"] , ["Users","account"] , ["Portfolio","Servive","Centre"]] ;
-
-// for (let i = 0 ; i < sidebarSections.length ; i++) {
-//     sidebarSections[i].addEventListener("click" ,() => {
-//             if (sidebarSections[i].classList.contains("disabled")){
-//                 sidebarSections.forEach((el) => {
-//                     el.classList.remove("active") ;
-//                     el.classList.add("disabled") ;
-//                 }) 
-//                 Sections.forEach((el) => {
-//                     el.innerHTML = "" ;
-//                 })
-//                 sidebarSections[i].classList.remove("disabled") ;
-//                 sidebarSections[i].classList.add("active") ;
-
-//                 const ul = document.createElement("ul");
-//                 ul.className = "drop-down" ;
-//                 Sections[i].appendChild(ul);
-//                 for (let j = 0 ; j < Dropdown[i].length ; j++) {
-//                 const li = document.createElement("li");
-//                 const a = document.createElement("a");
-//                 a.href = "#" ;
-//                 const text = document.createTextNode(Dropdown[i][j]);
-//                 a.appendChild(text);
-//                 li.appendChild(a);
-//                 ul.appendChild(li);
-//                 }
-//             } else {
-//                 sidebarSections[i].classList.remove("active") ;
-//                 sidebarSections[i].classList.add("disabled") ;
-//                 Sections[i].innerHTML = "" ;
-//             }
-
-
-
-
-
-        
-//     }) ;
-// }
-
-const pagesSection = document.querySelector(".pages");
-const reservationsSection = document.querySelector(".reservations");
-const authenticationSection = document.querySelector(".authentication");
-
-const pagesList = pagesSection.nextElementSibling;
-const reservationsList = reservationsSection.nextElementSibling;
-const authenticationList = authenticationSection.nextElementSibling;
-
-let pS = true,
-  pR = true,
-  pA = true;
-pagesSection.addEventListener("click", () => {
-  if (pS) {
-    pagesList.style.display = "flex";
-    pS = false;
-  } else {
-    pagesList.style.display = "none";
-    pS = true;
-  }
-});
-reservationsSection.addEventListener("click", () => {
-  if (pR) {
-    reservationsList.style.display = "flex";
-    pR = false;
-  } else {
-    reservationsList.style.display = "none";
-    pR = true;
-  }
-});
-authenticationSection.addEventListener("click", () => {
-  if (pA) {
-    authenticationList.style.display = "flex";
-    pA = false;
-  } else {
-    authenticationList.style.display = "none";
-    pA = true;
-  }
-});
-
+const navSections = document.querySelectorAll(".nav-section");
+const down_icons = document.querySelectorAll(".fa-chevron-down");
+const nextElements = Array.from(navSections).map(
+  (section) => section.nextElementSibling
+);
 const selections = document.querySelectorAll(".sub-title");
-let previous = document.querySelector(".all-appoint");
+
+let rotateCdts = Array(down_icons.length).fill(true);
+let visibilityCdts = Array(nextElements.length).fill(true);
+let previous = document.querySelector(".dashboard");
+let previousSec = document.querySelector(".dashboard");
+previousSec.style.fontWeight = "600";
+//Navigation sections having sub titles
+navSections.forEach((navSection, index) => {
+  //Only nav sections having subtitles under
+  if (index !== 0) {
+    navSection.addEventListener("click", () => {
+      //for visibility of  the elemts under
+      if (visibilityCdts[index]) {
+        nextElements[index].style.display = "flex";
+        visibilityCdts[index] = false;
+      } else {
+        nextElements[index].style.display = "none";
+        visibilityCdts[index] = true;
+      }
+      //for rotating the arrow
+      if (rotateCdts[index - 1]) {
+        down_icons[index - 1].style.transform = "rotate(180deg)";
+        rotateCdts[index - 1] = false;
+      } else {
+        down_icons[index - 1].style.transform = "rotate(0deg)";
+        rotateCdts[index - 1] = true;
+      }
+    });
+  } else {
+    navSection.addEventListener("click", () => {
+      previousSec.style.fontWeight = "normal";
+      navSection.style.fontWeight = "600";
+      previous.style.fontWeight = "normal";
+      previousSec = navSection;
+    });
+  }
+});
+
+//keep color of selected subtitle
 selections.forEach((element) => {
   element.addEventListener("click", () => {
-    previous.style.backgroundColor = "transparent";
+    // Font Weight Of selected subtitle's Parent Title
+    const navSection =
+      element.parentNode.parentNode.querySelector(".nav-section");
+    previousSec.style.fontWeight = "normal";
+    navSection.style.fontWeight = "600";
+    previousSec = navSection;
+    //backgroung color of selected subtitle
     element.style.backgroundColor = "#f0f0f0";
+    element.style.fontWeight = "bold";
+    previous.style.backgroundColor = "transparent";
+    previous.style.fontWeight = "normal";
     previous = element;
-    element.classList.add("sub-title");
   });
 });
 
-const burgerMenu = document.getElementById("burger-menu");
-
-burgerMenu.addEventListener("click", () => {
-  burgerMenu.classList.toggle("toggled");
-});
+//hiding SideBar
+// header_pane.addEventListener("click", () => {
+//   titlesText.forEach((title) => {
+//     setInvisible(title);
+//   });
+//   titlesIcon.forEach((icon) => {
+//     icon.style.fontSize = "2rem";
+//     icon.style.margin = "auto";
+//   });
+//   down_icons.forEach((icon) => {
+//     icon.style.display = "none";
+//   });
+//   navSections.forEach((navSection) => {
+//     navSection.style.height = "fit-content";
+//     navSection.style.width = "100%";
+//   });
+//   titles.forEach((title) => {
+//     title.style.width = "5rem";
+//     title.style.height = "5rem";
+//     // title.style.boxShadow = "0px 0px 6px #dedede";
+//     // title.style.padding = "20%";
+//     title.style.margin = "auto";
+//   });
+//   navigation_title.textContent = "";
+//   profile_img.style.margin = "auto";
+//   profile_img.style.width = "3.5rem";
+//   admin_data.style.display = "none";
+//   navigationBar.style.width = "8%";
+//   contentSection.style.width = "90%";
+// });
